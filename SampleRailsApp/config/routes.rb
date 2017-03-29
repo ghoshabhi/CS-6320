e93fcf278cdda53b535b8540b17a14b9d18547ca
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-29 00:56:34 -0700

    Update: Update routes

--- SampleRailsApp/config/routes.rb
+++ SampleRailsApp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'say/hello'
+  get 'say/hello' => 'say#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
