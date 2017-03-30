ce0dc3c04df4c1eb9e4a3d7555dd9f51b5a9879f
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-30 12:50:27 -0700

    Update

--- SampleRailsApp/app/controllers/say_controller.rb
+++ SampleRailsApp/app/controllers/say_controller.rb
@@ -1,4 +1,18 @@
 class SayController < ApplicationController
   def hello
   end
+
+  def greet
+    @name = params[:name]
+  end
+
+  def photo
+    @id = params[:id]
+    @sample_json = {name: 'Abhi', last_name: 'Ghosh', id: @id}
+    # respond_to.as_json { @sample_json }
+    respond_to do |format|
+      format.json { render json: @sample_json }
+      format.html
+    end
+  end
 end
--- SampleRailsApp/config/routes.rb
+++ SampleRailsApp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'say/hello' => 'say#hello'
-
+  get 'say/greet' => 'say#greet'
+  get '/photo/:id' => 'say#photo', as: 'get_photo'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
